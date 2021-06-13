f534538bc5933a276ccc3447e12ff7486b3a895b
life-f <58433392+life-f@users.noreply.github.com>
2021-06-13 20:32:10 +0300

    fix slider

--- win-factory/js/window.js
+++ win-factory/js/window.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     $('.services-slider').slick({
         variableWidth: false,
-        centerMode: true,
-        centerPadding: '80px',
+        // centerMode: true,
+        // centerPadding: '80px',
         arrows: true,
         prevArrow: '<div class="arrow-prev arrow"><img src="../img/window/prev-arrow.svg"></div>',
         nextArrow: '<div class="arrow-next arrow"><img src="../img/window/prev-arrow.svg"></div>',
