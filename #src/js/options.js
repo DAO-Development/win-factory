51c4869296bfa821013a85cec767f58f53711d94
life-f <58433392+life-f@users.noreply.github.com>
2021-06-27 20:21:30 +0300

    add js for pop-up

--- #src/js/options.js
+++ #src/js/options.js
@@ -42,4 +42,20 @@ $(document).ready(function () {
             slidesToShow: 3.1,
         });
     }
+
+    let cards = document.getElementsByClassName('options__card')
+    let wall = document.getElementsByClassName('pop-up__wall')
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].onclick = function () {
+            console.log(cards[i].id)
+            let popup = document.getElementById('pop-up-' + (i + 1))
+            popup.classList.add('active')
+            wall[0].classList.add('active')
+        }
+    }
+
+    $('.pop-up__close-btn').on("click", function () {
+        $('.options__pop-up').removeClass('active')
+        $('.pop-up__wall').removeClass('active')
+    })
 })
