8b31f0fb526a50a9c2b758d42554cab560effeaf
life-f <58433392+life-f@users.noreply.github.com>
2021-06-30 18:52:56 +0300

    zoom examples final

--- #src/js/examples.js
+++ #src/js/examples.js
@@ -33,9 +33,15 @@ $(document).ready(function () {
             var i_path = $(this).find('img').attr('src');
             $('body').append('<div id="overlay"></div><div id="magnify"><img src="' + i_path + '"><div id="close-popup"><i></i></div></div>');
             $('#magnify').css({
-                left: ($(document).width() - $('#magnify').outerWidth()) / 2,
+                // left: ($(document).width() - $('#magnify').outerWidth()) / 2,
                 // top: ($(document).height() - $('#magnify').outerHeight())/2 upd: 24.10.2016
-                top: ($(window).height() - $('#magnify').outerHeight()) / 2
+                // top: ($(window).height() - $('#magnify').outerHeight()) / 2
+                left: 0,
+                top: 0,
+                display: 'grid',
+                placeSelf: 'center',
+                gridTemplateColumns: '100%',
+                gridTemplateAreas: '100%',
             });
             $('#overlay, #magnify').fadeIn('fast');
         });
