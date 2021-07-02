3c1758fee6f65feb2cdbcfbaa5c45bc1f0800562
life-f <58433392+life-f@users.noreply.github.com>
2021-07-02 22:00:06 +0300

    fixes sliders and other bugs

--- #src/js/balconies.js
+++ #src/js/balconies.js
@@ -1,42 +1,35 @@
 $(document).ready(function () {
-    console.log($(window).width());
-    if ($(window).width() <= '768') {
-
+    if ($(window).width() <= '360') {
         $('.products__content').slick({
             variableWidth: false,
             arrows: false,
             dots: true,
             infinite: false,
-            responsive: [
-                {
-                  breakpoint: 769,
-                  settings: {
-                    slidesToShow: 2.3,
-                  }
-                },
-                {
-                  breakpoint: 569,
-                  settings: {
-                    slidesToShow: 2.07,
-                  }
-                },
-                {
-                    breakpoint: 415,
-                    settings: {
-                      slidesToShow: 1.5,
-                    }
-                },
-                {
-                    breakpoint: 361,
-                    settings: {
-                      slidesToShow: 1.4,
-                    }
-                }
-                
-            ]
+            slidesToShow: 1.4,
+        })
+    } else if ($(window).width() <= '414') {
+        $('.products__content').slick({
+            variableWidth: false,
+            arrows: false,
+            dots: true,
+            infinite: false,
+            slidesToShow: 1.5,
+        })
+    } else if ($(window).width() <= '568') {
+        $('.products__content').slick({
+            variableWidth: false,
+            arrows: false,
+            dots: true,
+            infinite: false,
+            slidesToShow: 2.07,
+        })
+    } else if ($(window).width() <= '768') {
+        $('.products__content').slick({
+            variableWidth: false,
+            arrows: false,
+            dots: true,
+            infinite: false,
+            slidesToShow: 2.3,
         });
     }
-
-
-    
 })
--- #src/js/colors.js
+++ #src/js/colors.js
@@ -1,49 +1,45 @@
 $(document).ready(function () {
     console.log($(window).width());
-    if ($(window).width() <= '768') {
+    if ($(window).width() <= '360') {
+        $('.colors__show').slick({
+            variableWidth: false,
+            arrows: false,
+            dots: true,
+            infinite: false,
+            slidesToShow: 1.4,
+        })
+    } else if ($(window).width() <= '414') {
+        $('.colors__show').slick({
+            variableWidth: false,
+            arrows: false,
+            dots: true,
+            infinite: false,
+            slidesToShow: 1.5,
+        })
+    } else if ($(window).width() <= '568') {
+        $('.colors__show').slick({
+            variableWidth: false,
+            arrows: false,
+            dots: true,
+            infinite: false,
+            slidesToShow: 2.07
+        })
+    } else if ($(window).width() <= '768') {
 
         $('.colors__show').slick({
             variableWidth: false,
             arrows: false,
             dots: true,
             infinite: false,
-            responsive: [
-                {
-                    breakpoint: 769,
-                    settings: {
-                      slidesToShow: 2.1,
-                    }
-                  },
-                {
-                  breakpoint: 569,
-                  settings: {
-                    slidesToShow: 2.07,
-                  }
-                },
-                {
-                    breakpoint: 415,
-                    settings: {
-                      slidesToShow: 1.5,
-                    }
-                },
-                {
-                    breakpoint: 361,
-                    settings: {
-                      slidesToShow: 1.4,
-                    }
-                }
-                
-            ]
+            slidesToShow: 2.1,
         });
-    }
-
-    else{
+    } else {
         let colors = document.getElementsByClassName('colors__color');
         let items = document.getElementsByClassName('colors__item')
         let last_item = 0
 
-        for (let i = 0; i<colors.length; i++){
-            colors[i].addEventListener("click", function(){
+        for (let i = 0; i < colors.length; i++) {
+            colors[i].addEventListener("click", function () {
                 colors[last_item].classList.remove("active")
                 colors[i].classList.add("active")
                 items[i].classList.remove("invis")
@@ -55,5 +51,4 @@ $(document).ready(function () {
     }
 
 
-    
 })
